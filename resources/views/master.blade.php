c06f500922ffc66505ccd1378922df81d1cc3742
Berkan Kütük <berkank@live.dk>
2021-11-13 17:10:24 +0100

    Finished "Check if logged in"

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -48,8 +48,10 @@
 
                 <!-- Task 1 Authorization, elements should appear for logged users only -->
                     <!-- Task 1 User, step 1: add name of logged user-->
-                    <span class="navbar-text text-black me-4 user-name">user name</span>
-                    <li class="nav-item">
+                @auth()
+                <span class="navbar-text text-black me-4 user-name">{{auth()->user()->name}}</span>
+                @endauth
+                <li class="nav-item">
                         <!-- Task 2 User, step 3: add correct link-->
                         <a class="nav-link logout-link" href="#">Log out</a>
                     </li>
